9bc5fb9f4d39d92a3b3f732c741086e3c114e446
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-09-16 11:35:37 +0530

    Bug fix

--- main.py
+++ main.py
@@ -208,8 +208,8 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-        del checkpoint['model']['class_embed.weight']
-        del checkpoint['model']['class_embed.bias']
+        # del checkpoint['model']['class_embed.weight']
+        # del checkpoint['model']['class_embed.bias']
         # del checkpoint['model']['query_embed.weight']
         model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
         if not args.eval and 'discriminator_model' in checkpoint:
