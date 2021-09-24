5c8891cbe67bf85b82912b4d1feefea5494299f8
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-09-24 12:11:39 +0530

    Generate features completed

---
+++ util/configuration.py
@@ -0,0 +1,39 @@
+# DATASET_DIR = '/home/cabe0006/mb20_scratch/chamath/cvpr_experiments/cvpr_data'
+DATASET_DIR = '/Users/cabe0006/Projects/monash/cvpr_data'
+VIDEO_CLIPS = {
+    # 'train': ['OU10B3L2In_0', 'OU50B1L1In_0', 'OU50B1L1Out_0', 'OU10B1L1Out_0',
+    #           'OU10B3L3In_0', 'OU10B2L2In_0', 'OU50B1L2In_0', 'OU10B2L3Out_0',
+    #           'OU10B2L2Out_0', 'OU50B2L2Out_0', 'OU50B1L3In_0', 'OU10B2L3In_0',
+    #           'OU50B1L3Out_0', 'OU50B1L2Out_0', 'OU10B1L2In_0', 'OU50B2L2In_0',
+    #           'OU10B1L3Out_0', 'OU10B1L2Out_0', 'OU50B2L3In_0', 'OU10B1L3In_0',
+    #           'OU10B3L2Out_0', 'OU50B3L2In_0', 'OU10B3L3Out_0', 'OU50B3L2Out_0',
+    #           'OU50B3L3Out_0', 'OU50B2L1In_0', 'OU10B1L1In_0', 'OU50B3L3In_0',
+    #           'OU10B3L1In_0', 'OU50B3L1In_0', 'OU10B2L1In_0', 'OU10B2L1Out_0',
+    #           'OU10B3L1Out_0'],
+    'train': ['OU10B3L2In_0', 'OU50B1L1In_0',
+              'OU10B3L3In_0', 'OU50B1L2In_0',
+              'OU10B1L3Out_0', 'OU50B3L2Out_0',
+              'OU10B3L3Out_0', 'OU50B2L2In_0'],
+    'val': ['CU50L1B6Out_0', 'CU50L1B6In_0',
+            'CU10L1B5Out_0', 'CU10L1B5In_0',
+            'CU25L1B4Out_0', 'CU25L1B4In_0',
+            'CU25L1B1Out_0', 'CU25L1B1In_0'],
+    # 'test_minified': [
+    #     'CU30L1B6Out_0', 'CU30L1B6In_0', 'CU10L1B6In_0', 'CU10L1B6Out_0', 'CU50L1B6Out_0', 'CU50L1B6In_0',
+    #     'CU10L1B5Out_0', 'CU10L1B5In_0',
+    #     'CU25L1B4In_0', 'CU25L1B4Out_0', 'CU20L1B4Out_0',
+    #     'CU10L1B1In_0', 'CU10L1B1Out_0'
+    # ],
+    'test_minified': ['CU50L1B6Out_0', 'CU50L1B6In_0',
+            'CU10L1B5Out_0', 'CU10L1B5In_0',
+            'CU25L1B4Out_0', 'CU25L1B4In_0',
+            'CU25L1B1Out_0', 'CU25L1B1In_0'],
+    'test': ['CU30L1B6Out_0', 'CU10L1B6In_0', 'CU10L1B4In_0', 'CU25L1B1In_0', 'CU20L1B4Out_0',
+             'CU15L1B1Out_0', 'CU10L1B4Out_0', 'CU10L1B5Out_0', 'CU25L1B1Out_0',
+             'CU20L1B1In_0', 'CU15L1B4In_0', 'CU10L1B5In_0', 'CU20L1B1Out_0',
+             'CU15L1B4Out_0', 'CU20L1B4In_0', 'CU15L1B1In_0', 'CU10L1B1Out_0',
+             'CU25L1B4Out_0', 'CU10L1B1In_0', 'CU25L1B4In_0', 'CU30L1B6In_0',
+             'CU10L1B6Out_0', 'CU50L1B6Out_0', 'CU50L1B6In_0'],
+    'train_small': ['OU10B2L2Out_0', 'OU50B2L2Out_0'],
+    'test_small': ['CU10L1B6Out_0', 'CU50L1B6Out_0']
+}
