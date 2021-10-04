00681cc712726305d68dedf17f2a64e2192868f7
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-10-04 09:41:02 +0530

    bug fix

--- train_evaluation.py
+++ train_evaluation.py
@@ -193,9 +193,7 @@ def main(args):
 
         if args.eval:
             test_stats, coco_evaluator = evaluate(
-                model, criterion, discriminator_model, discriminator_criterion,
-                postprocessors, data_loader_train, base_ds, device, args.output_dir,
-                args.batch_size
+                model, criterion, postprocessors, data_loader_train, base_ds, device, args.output_dir
             )
             if args.output_dir:
                 utils.save_on_master(coco_evaluator.coco_eval["bbox"].eval, output_dir / "eval.pth")
