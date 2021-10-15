f8d31aa218d201df5c69579d19500dc910aa5580
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-10-15 12:15:00 +0530

    change scale

--- datasets/ant2.py
+++ datasets/ant2.py
@@ -204,11 +204,11 @@ def get_train_transforms_torch():
     return T.Compose([
         T.RandomHorizontalFlip(),
         T.RandomSelect(
-            T.RandomResize(scales, max_size=1333),
+            T.RandomResize([(1024, 542)], max_size=1333),
             T.Compose([
                 T.RandomResize([400, 500, 600]),
                 T.RandomSizeCrop(384, 600),
-                T.RandomResize(scales, max_size=1333),
+                T.RandomResize([(1024, 542)], max_size=1333),
             ])
         ),
         normalize,
