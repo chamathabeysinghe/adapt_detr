1617b45abb26d8615b4284237e67410a26c8b080
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-10-15 12:22:11 +0530

    script changes

--- scripts.bash
+++ scripts.bash
@@ -73,6 +73,8 @@ python visualizer.py --device cpu --dataset_file ant --data_path /Users/cabe0006
 /home/cabe0006/mb20_scratch/chamath/detr/venv_detr/bin/python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --dataset_file ant2 --data_path /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/cvpr_data/detection_dataset --output_dir /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/checkpoints --resume /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/checkpoints/detr-r50-e632da11.pth
 
 ###################################################Nature Journal Trainings############################################################
+/home/cabe0006/mb20_scratch/chamath/detr/venv_detr/bin/python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --dataset_file ant2 --data_path /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/cvpr_data/detection_dataset_nature --output_dir /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/checkpoints_nature --resume /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/checkpoints/detr-r50-e632da11.pth
+
 /home/cabe0006/mb20_scratch/chamath/detr/venv_detr/bin/python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --dataset_file ant2 --data_path /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/cvpr_data/detection_dataset --output_dir /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/checkpoints_batch --resume /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/checkpoints_batch/checkpoint_0199.pth --eval
 
 
