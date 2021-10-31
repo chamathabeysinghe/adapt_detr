ca67502b25771eb3623e95505df68606b95a3144
chamathabeysinghe <abeysinghechamath@gmail.com>
2021-10-31 17:55:11 +1100

    Bug fix

--- engine.py
+++ engine.py
@@ -31,7 +31,8 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
 
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
         samples = samples.to(device)
-        val_samples, _ = next(target_img_iter).to(device)
+        val_samples, _ = next(target_img_iter)
+        val_samples = val_samples.to(device)
         # samples_val, targets_val = next(data_loader_val_iter)  # TODO Create new data generator
         # samples_val = samples_val.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
