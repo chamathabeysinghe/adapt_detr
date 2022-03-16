2a0957840b15a25578972b47f4859fa049f5d970
chamathabeysinghe <abeysinghechamath@gmail.com>
2022-03-17 09:32:29 +1100

    Data generation scripts

--- evaluation_coco/eval.py
+++ evaluation_coco/eval.py
@@ -3,8 +3,8 @@ from pycocotools.cocoeval import COCOeval
 import os
 
 annType = 'bbox'
-cocoGt = COCO('./ground-truth-test.json')
-cocoDt = cocoGt.loadRes('./eval-results.json')
+cocoGt = COCO('/Users/cabe0006/Projects/monash/cvpr_experiments/nature_journal_publication/predictions_rcnn_test_5/coco_format/ground-truth-test.json')
+cocoDt = cocoGt.loadRes('/Users/cabe0006/Projects/monash/cvpr_experiments/nature_journal_publication/predictions_rcnn_test_5/coco_format/test-predictions-test.json')
 cocoEval = COCOeval(cocoGt, cocoDt, annType)
 imgIds = sorted(cocoGt.getImgIds())
 cocoEval.params.imgIds = imgIds
