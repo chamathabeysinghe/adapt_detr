d9c347ea8ee745d639f556e987248fb0c3a83a00
chamathabeysinghe <abeysinghechamath@gmail.com>
2022-03-23 10:31:48 +1100

    Delete weights on initialization only

--- main.py
+++ main.py
@@ -101,6 +101,8 @@ def get_args_parser():
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
     # experiment names
     parser.add_argument('--name', type=str, required=True)
+    parser.add_argument('--init', action='store_true')
+
     return parser
 
 
@@ -184,9 +186,10 @@ def main(args):
                 args.resume, map_location='cpu', check_hash=True)
         else:
             checkpoint = torch.load(args.resume, map_location='cpu')
-        del checkpoint['model']['class_embed.weight']
-        del checkpoint['model']['class_embed.bias']
-        # del checkpoint['model']['query_embed.weight']
+        if args.init:
+            del checkpoint['model']['class_embed.weight']
+            del checkpoint['model']['class_embed.bias']
+            # del checkpoint['model']['query_embed.weight']
         model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
