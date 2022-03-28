70b96893959d79b72b7690f934b35bea128a7d32
chamathabeysinghe <abeysinghechamath@gmail.com>
2022-03-28 13:42:58 +1100

    Strong feaure alignment experiment 5

--- execute.sh
+++ execute.sh
@@ -2,10 +2,9 @@
 				--dataset_file ant2 \
 				--data_path /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/cvpr_data/detection_source_dataset_small \
 				--data_path_target /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/cvpr_data/detection_target_dataset_small \
-				--output_dir /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/detr_disc_exp4_small_dataset_coef_100 \
+				--output_dir /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/detr_disc_exp5_small_dataset_coef_100_exp2weights \
 				--batch_size 2 \
-				--resume /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/original_paper_checkpoints/detr-r101-2c7b67e5.pth \
+				--resume /home/cabe0006/mb20_scratch/chamath/cvpr_experiments/detr_output/detr_disc_exp5_small_dataset_coef_100_exp2weights/checkpoint.pth \
 				--backbone resnet101 \
-				--name detr_disc_exp4_small_dataset_coef_100 \
-				--init \
+				--name detr_disc_exp5_small_dataset_coef_100_exp2weights \
 				--disc_loss_coef 100
