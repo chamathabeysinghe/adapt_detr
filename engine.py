aaeb5edc805b52ae8555835e6eb71d4d79896418
chamathabeysinghe <abeysinghechamath@gmail.com>
2022-04-01 09:56:19 +1100

    Eval changes

--- engine.py
+++ engine.py
@@ -130,7 +130,7 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         samples = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        outputs, _, _ = model(samples)
+        outputs, _, _, _ = model(samples)
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
 
