faa4e595350a8057572e2deeb1086aaf213ff12b
chamathabeysinghe <abeysinghechamath@gmail.com>
2023-03-27 10:49:45 +0530

    train on target

--- loop.sh
+++ loop.sh
@@ -1,15 +1,17 @@
 python main.py  \
 --dataset_file ant2 \
---data_path /dice1-data/home/cabe0006/cvpr_experiments/cvpr_data/detection_source_dataset \
+--data_path /dice1-data/home/cabe0006/cvpr_experiments/cvpr_data/detection_target_dataset \
 --data_path_target /dice1-data/home/cabe0006/cvpr_experiments/cvpr_data/task_switching_dataset \
---output_dir /dice1-data/home/cabe0006/cvpr_experiments/detr_output/detr_task_switching \
+--output_dir /dice1-data/home/cabe0006/cvpr_experiments/detr_output/detr_task_switching_target \
 --batch_size 2 \
 --resume /dice1-data/home/cabe0006/cvpr_experiments/detr_output/detr_exp25/checkpoint_best_ap_50.pth \
 --backbone resnet101 \
 --name detr-task-switching-exp \
 --init \
 --disc_loss_coef_local 100 \
---disc_loss_coef_global 100
+--disc_loss_coef_global 100 \
+--checkpoint_freq 1
+
 
 
 
--- loop3.sh
+++ loop3.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #for i in CU10L1B1In_0 CU10L1B1Out_0 CU10L1B4In_0 CU10L1B4Out_0 CU10L1B5In_0 CU10L1B5Out_0 CU15L1B1In_0 CU15L1B1Out_0 CU15L1B4In_0 CU15L1B4Out_0 OU10B1L1In_0 OU10B1L1Out_0 OU10B1L2In_0 OU10B1L2Out_0 OU10B1L3In_0 OU10B1L3Out_0 OU10B2L1In_0 OU10B2L1Out_0 OU10B2L2In_0 OU10B2L2Out_0 OU10B2L3In_0 OU10B2L3Out_0 OU10B3L1In_0 OU10B3L1Out_0 OU10B3L2In_0 OU10B3L2Out_0 OU10B3L3In_0 OU10B3L3Out_0
-for i in CU10L1B6In_0 CU15L1B1In_0 CU15L1B4In_0 CU20L1B1Out_0 CU20L1B4Out_0 CU30L1B6Out_0
+#for i in CU10L1B6In_0 CU15L1B1In_0 CU15L1B4In_0 CU20L1B1Out_0 CU20L1B4Out_0 CU30L1B6Out_0
+for i in task_switching_test
 do
-#  /home/cabe0006/mb20_scratch/chamath/detr/venv_detr/bin/python -m torch.distributed.launch --nproc_per_node=1 --use_env visualizer.py --dataset_file ant --data_path /home/cabe0006/mb20_scratch/chamath/data/ant_dataset_images/$i --output_dir /home/cabe0006/mb20_scratch/chamath/data/ant_dataset_medium_image_predictions/$i --resume /home/cabe0006/mb20_scratch/chamath/detr-v4/checkpoints/checkpoint.pth --thresh 0.75
-  /dice1-data/home/cabe0006/Projects/py_environments/env/bin/python -m torch.distributed.launch --nproc_per_node=1 --use_env visualizer.py --dataset_file ant --data_path /dice1-data/home/cabe0006/cvpr_experiments/trackformer_output/predictions/input/$i --output_dir /dice1-data/home/cabe0006/cvpr_experiments/trackformer_output/predictions/detr_detections_test/$i --resume /dice1-data/home/cabe0006/cvpr_experiments/detr_output/detr_exp25/checkpoint0259.pth --thresh 0.60
+  /dice1-data/home/cabe0006/Projects/py_environments/env/bin/python -m torch.distributed.launch --nproc_per_node=1 --use_env visualizer.py \
+  --dataset_file ant \
+  --data_path /dice1-data/home/cabe0006/cvpr_experiments/trackformer_output/predictions/input/$i \
+  --output_dir /dice1-data/home/cabe0006/cvpr_experiments/trackformer_output/predictions/detr_detections_test/$i \
+  --resume /dice1-data/home/cabe0006/cvpr_experiments/detr_output/detr_task_switching/checkpoint0029.pth \
+  --thresh 0.94
 
-
-#  python test.py --value=$i
-#  python -c 'print "a"*'$i
 done
